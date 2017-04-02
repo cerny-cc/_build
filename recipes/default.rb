caba09e2f4bd05d2f5a5d90965fb55b9db16a05a
Nathan Cerny <ncerny@gmail.com>
2017-04-02 16:07:22 -0500

    Set up ssh keys for build nodes.
    Signed-off-by: Nathan Cerny <ncerny@gmail.com>

--- recipes/default.rb
+++ recipes/default.rb
@@ -15,6 +15,22 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# F75ruqePSTN*MqQouNm^Y&mdHsLg2uS8$2SP&K9zNW&Mc*SZ!VN%S@K5HAnE4zT8JI%xsTO6b3cRYw*Zn!zdmSg&gS3n@9gH3
+
+directory '/var/opt/delivery/workspace/.ssh' do
+  owner 'dbuild'
+  group 'dbuild'
+  mode '0700'
+end
+
+execute 'Generate SSH keys' do
+  user 'dbuild'
+  command "ssh-keygen -q -f /var/opt/delivery/workspace/.ssh/id_rsa -N ''"
+  creates '/var/opt/delivery/workspace/.ssh/id_rsa'
+  action :nothing
+end.run_action(:run)
+
+node.default['ssh']['keys']['dbuild'] = ::File.read('/var/opt/delivery/workspace/.ssh/id_rsa.pub')
 
 yum_repository 'packages-microsoft-com-prod' do
   description 'Microsoft Prod'
