482eb1bc0721fb9ee7959f7421b69dbb1c976a71
Nathan Cerny <ncerny@gmail.com>
2017-04-15 11:07:39 -0500

    try reversing order
    Signed-off-by: Nathan Cerny <ncerny@gmail.com>

--- recipes/syntax.rb
+++ recipes/syntax.rb
@@ -78,7 +78,7 @@ DeliverySugar::ChefServer.new(delivery_knife_rb).with_server_config do
   Chef::Log.error('Prior to merge')
   Chef::Log.error(deps)
   Chef::Log.error(external)
-  external.raw_data = Chef::Mixin::DeepMerge.deep_merge!(deps, external)
+  external.raw_data = Chef::Mixin::DeepMerge.deep_merge!(external, deps)
   Chef::Log.error('After merge')
   Chef::Log.error(external)
 
