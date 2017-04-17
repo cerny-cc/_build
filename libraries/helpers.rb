aa3efb3ffc24df4dc7e4a6c9837c4cb563026333
Nathan Cerny <ncerny@gmail.com>
2017-04-16 20:21:45 -0500

    pretty generate json, and fix git commit message.
    Signed-off-by: Nathan Cerny <ncerny@gmail.com>

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -51,3 +51,11 @@ def delivery_api(method = :get, path = '/_status', data = '')
     (http_client.send method, request_url, headers).body
   end
 end
+
+def external_cookbooks_json
+  hash = Chef::Mixin::DeepMerge.deep_merge(JSON.parse(::File.read("#{cookbook_directory}/_pipeline/external_cookbooks.json")), deps)
+  hash.each do |_, v|
+    v.sort
+  end
+  JSON.pretty_generate(hash)
+end
